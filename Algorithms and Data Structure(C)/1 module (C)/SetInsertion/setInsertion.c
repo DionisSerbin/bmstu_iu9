6b6ac22efd5d3eb42ace3a47c6aef9ab487f51df
DionisSerbin <67962669+DionisSerbin@users.noreply.github.com>
2022-09-01 21:46:45 +0300

    Delete setInsertion.c

--- Algorithms and Data Structure(C)/1 module (C)/SetInsertion/setInsertion.c
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/Praepostor-IU9/IU9_Server/blob/master/Server/Semestr_1/Modul_1/04/main.c</string>
-</dict>
-</plist>
